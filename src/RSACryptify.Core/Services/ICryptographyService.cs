50abcce0b9ff859683d232b9b20f2e8e15e85886
hasan  khaddour <hasan.b.khaddour@gmail.com>
2024-12-31 17:10:40 +0300

    Add Cryptography Service

---
+++ src/RSACryptify.Core/Services/ICryptographyService.cs
@@ -0,0 +1,12 @@
+﻿using RSACryptify.Core.Abstraction;
+using System;
+
+namespace RSACryptify.Core.Services
+{
+    public interface ICryptographyService
+    {
+        public string Encrypt(string plainText, Key publickKey);
+        public string Decrypt(string cipherText, Key privateKey);
+
+    }
+}
