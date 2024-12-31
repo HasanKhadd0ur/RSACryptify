85716fbceff9bb3d98cfc3df4ce75c0c25d40068
hasan  khaddour <hasan.b.khaddour@gmail.com>
2024-12-31 17:10:19 +0300

    Add Key and Key Pair abstraction

---
+++ src/RSACryptify.Core/Abstraction/KeyPair.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSACryptify.Core.Abstraction
+{
+    public class KeyPair
+    {
+        public BigInteger PublicKeyExponent { get; set; } // Public key exponent (e)
+        public BigInteger PrivateKeyExponent { get; set; } // Private key exponent (d)
+        public BigInteger Modulus { get; set; } // Modulus (n)
+    }
+
+}
+
