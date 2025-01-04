a22ba88ce3eb615309a2db5f97cc9d711ae743f7
hasan  khaddour <hasan.b.khaddour@gmail.com>
2025-01-04 20:52:27 +0300

    Fix Something

---
+++ src/RSACryptify.Application/utilities/RandomNumberService.cs
@@ -0,0 +1,35 @@
+﻿using RSACryptify.Core.utilities;
+using System;
+using System.Numerics;
+
+namespace RSACryptify.Application
+{
+    public class RandomNumberService : IRandomNumberService
+    {
+        private readonly Random _random = new Random();
+
+        public BigInteger GeneratePrimeNumber(BigInteger min, BigInteger max)
+        {
+            while (true)
+            {
+                var candidate = GenerateRandomNumber(min, max);
+                if (IsPrime(candidate)) return candidate;
+            }
+        }
+
+        public BigInteger GenerateRandomNumber(BigInteger min, BigInteger max)
+        {
+            return new BigInteger(_random.Next((int)min, (int)max));
+        }
+
+        private bool IsPrime(BigInteger number)
+        {
+            if (number < 2) return false;
+            for (BigInteger i = 2; i <= BigInteger.Pow(number, 1 / 2); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
